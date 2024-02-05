f271172f0e5f10197bcaba195288c44c98cc3f79
Henry Agu <aguhenrychuks@gmail.com>
2024-02-05 20:48:38 +0100

    feat: navbar updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// components
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+
+  return (
+    <section className="bg-black h-screen font-inter">
+      <Navbar/>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+// react icons
+import { RiMenu3Fill } from "react-icons/ri";
+import { MdClose } from "react-icons/md";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <section>
+      {/* Desktop */}
+      <div className="text-white p-5 xl:p-8 xl:w-4/5 xl:my-0 xl:mx-auto xl:flex xl:items-center xl:justify-between">
+        <div className="flex item-center justify-between">
+          <div>
+            <h1 className="font-semibold xl:text-xl xl:font-bold">JustAnotherUIKit</h1>
+          </div>
+          <div className="xl:hidden">
+            <button onClick={() => setOpenMenu(!openMenu)}>
+              {openMenu ? (
+                <MdClose className="text-3xl" />
+              ) : (
+                <RiMenu3Fill className="text-3xl" /> 
+              )}
+            </button>
+          </div>
+        </div>
+        <div className="hidden xl:block">
+          <ul className="xl:flex xl:items-center xl:justify-between xl:gap-x-8">
+            <li className="cursor-pointer xl:text-base xl:font-bold xl:leading-5">
+              Home
+            </li>
+            <li className="cursor-pointer xl:text-base xl:font-extralight xl:leading-5">
+              About
+            </li>
+            <li className="cursor-pointer xl:text-base xl:font-extralight xl:leading-5">
+              Contact
+            </li>
+            <li className="cursor-pointer xl:text-base xl:font-extralight xl:leading-5">
+              Blog
+            </li>
+          </ul>
+        </div>
+        {/* Mobile menu */}
+        {openMenu ? (
+          <div>
+            <ul className="flex flex-col gap-y-3">
+              <li className="cursor-pointer border-b-5 font-bold">
+                Home
+              </li>
+              <li className="cursor-pointer border-b-5">
+                About
+              </li>
+              <li className="cursor-pointer border-b-5">
+                Contact
+              </li>
+              <li className="cursor-pointer border-b-5">
+                Blog
+              </li>
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
